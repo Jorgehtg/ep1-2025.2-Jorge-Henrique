7077d294bb736da724a1f0a888459f3c10942a1d
Jorgehtg <jgargalhone@gmail.com>
2025-09-21 22:37:25 -0300

    Criando a classe paciente e seus metodos

---
+++ Projeto/src/Paciente.java
@@ -0,0 +1,58 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Paciente extends Pessoa {
+    private String cpf;
+    private int idade;
+    private List<String> consultas;
+    private List<String> internações;
+
+    public Paciente(){
+        super();
+        this.cpf = "";
+        this.idade = 0;
+        this.consultas = new ArrayList<>();
+        this.internações = new ArrayList<>();
+    }
+
+    public Paciente(String nome, String cpf, int idade){
+        super(nome);
+        this.cpf = cpf;
+        this.idade = idade;
+        this.consultas = new ArrayList<>();
+        this.internações = new ArrayList<>();
+    }
+
+    public String getCPF(){
+        return this.cpf;
+    }
+
+    public void setCPF(String cpf){
+        this.cpf = cpf;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public List<String> getConsultas(){
+        return this.consultas;
+    }
+
+    public void setConsultas(String consulta){
+        this.consultas.add(consulta);
+    }
+
+    public List<String> getInternações(){
+        return internações;
+    }
+
+    public void setInternações(String internação){
+        this.internações.add(internação);
+    }
+
+}
