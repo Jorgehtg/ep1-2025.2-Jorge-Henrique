2e6cab3979c10ae561cb0a2f7f4a603976878fda
Jorgehtg <jgargalhone@gmail.com>
2025-09-21 22:57:13 -0300

    Criando a classe medico e seus metodos

---
+++ Projeto/src/Medico.java
@@ -0,0 +1,70 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Medico extends Pessoa{
+    private String crm;
+    private String especialidade;
+    private double custoConsulta;
+    private List<String> agenda;
+
+    public Medico(){
+        super();
+        this.crm = "";
+        this.especialidade = "";
+        this.custoConsulta = 0.0;
+        this.agenda = new ArrayList<>();
+    }
+
+    public Medico(String nome, String crm, String especialidade, double custo){//medico com especialidade
+        super(nome);
+        this.crm = crm;
+        this.especialidade = especialidade;
+        this.custoConsulta = custo;
+        this.agenda = new ArrayList<>();
+    }
+
+    public Medico(String nome, String crm, double custo){//medico sem especialidade
+        super(nome);
+        this.crm = crm;
+        this.especialidade = "";
+        this.custoConsulta = custo;
+        this.agenda = new ArrayList<>();
+    }
+
+    public String getCRM(){
+        return this.crm;
+    }
+
+    public void setCRM(String crm){
+        this.crm = crm;
+    }
+
+    public String getEspecialidade(){
+        return this.especialidade;
+    }
+
+    public void setEspecialidade(String especialidade){
+        this.especialidade = especialidade;
+    }
+
+    public double getCustoConsulta(){
+        return custoConsulta;
+    }
+
+    public void setCustoConsulta(double custo){
+        this.custoConsulta = custo;
+    }
+
+    public List<String> getAgenda(){
+        return agenda;
+    }
+
+    public void addHorario(String hora){
+        this.agenda.add(hora);
+    }
+
+    public void removeHorario(String hora){
+        this.agenda.remove(hora);
+    }
+
+}
