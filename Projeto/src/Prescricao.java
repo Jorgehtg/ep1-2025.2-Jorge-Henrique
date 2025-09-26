f55c660878288d6da19a7562f6866e625d6c07b0
Jorgehtg <jgargalhone@gmail.com>
2025-09-26 19:11:43 -0300

    Criando a classe prescrição bem basica

---
+++ Projeto/src/Prescricao.java
@@ -0,0 +1,36 @@
+public class Prescricao{
+    private Medico medico;
+    private String remedio;
+    private int dias;
+
+    public Prescricao(Medico medico, String remedio, int dias){
+        this.medico = medico;
+        this.remedio = remedio;
+        this.dias = dias;
+    }
+
+    public Medico getMedico(){
+        return medico;
+    }
+
+    public String getRemedio(){
+        return this.remedio;
+    }
+
+    public void setRemedio(String remedio){
+        this.remedio = remedio;
+    }
+
+    public int getDias(){
+        return dias;
+    }
+
+    public void setDias(int dias){
+        this.dias = dias;
+    }
+
+    @Override
+    public String toString(){
+        return "Prescrição: " + medico.getNome() + "(CRM: " + medico.getCRM() + ") " + remedio;
+    }
+}
