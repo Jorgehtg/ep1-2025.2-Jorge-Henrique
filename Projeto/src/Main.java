5f26cde9176aa3844fec1e2eb72de1a67f3a15dc
Jorgehtg <jgargalhone@gmail.com>
2025-10-03 05:37:11 -0300

    Arrumando umas linhas q eu deixei tortas

--- Projeto/src/Main.java
+++ Projeto/src/Main.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
     static ArrayList<Paciente> pacientes = new ArrayList<>();
     static ArrayList<Medico> medicos = new ArrayList<>();
     static ArrayList<String> gerais = new ArrayList<>();
@@ -53,8 +52,8 @@ public class Main {
             System.out.println("Checar atendimentos marcados - 4");
             System.out.println("Checar histórico de consultas - 5");
             System.out.println("Checar histórico de internações - 6");
-            System.out.println("Para voltar à tela anterior - 0");
-            System.out.println("===========================================");
+            System.out.println("Voltar à tela anterior - 0");
+            System.out.println("============================================");
 
             int valor = input.nextInt();
             input.nextLine();
@@ -82,54 +81,51 @@ public class Main {
         }
     }
 
-    public static void CadastrarPaciente(Scanner input){
-        System.out.println("===========================================");
+    public static void CadastrarPaciente(Scanner input) {
+        System.out.println("============================================");
         System.out.print("Digite o nome do paciente: ");
         String nome = input.nextLine();
 
         System.out.print("Digite o cpf do paciente (formato 000000000-00): ");
         String cpf = "";
-        do{
+        while (true) {
             cpf = input.nextLine();
             if(!cpf.matches("\\d{9}-\\d{2}")){ 
                 System.out.println("Formato invalido! Use: 000000000-00");
                 continue;
-                }else{
-                    if(cpfs.contains(cpf)){
+            } else {
+                    if(cpfs.contains(cpf)) {
                         System.out.println("Paciente já cadastrado");
                         return;
                     }
                     break;
                 }
-        }while (true);
+        }
 
         int idade = 0;
-        do {
+        while (true) {
             System.out.print("Digite a idade do paciente: ");
-            
             if (input.hasNextInt()) {
                 idade = input.nextInt();
-                input.nextLine(); // Limpa o buffer
-                
-                // Verifica se a idade está em um range razoável
+                input.nextLine();
                 if (idade >= 0) {
                     break;
                 } else {
                     System.out.println("Digite um valor positivo");
                 }
             } else {
-                System.out.println("Digite apenas números para a idade!");
+                System.out.println("Digite apenas números");
                 input.nextLine();
             }
-        }while (true);
+        }
         
-        do{
+        while (true) {
             System.out.println("Tem plano de saúde? [S/N]");
             String res = input.nextLine().toUpperCase();
 
-            if(res.equalsIgnoreCase("S")){
+            if(res.equalsIgnoreCase("S")) {
                 break;
-            } else if(res.equals("N")){
+            } else if(res.equals("N")) {
                 Paciente p = new Paciente(nome, cpf, idade);
                 pacientes.add(p);
                 cpfs.add(cpf);
@@ -137,70 +133,70 @@ public class Main {
             } else {
                 System.out.println("Digite uma resposta valida!");
             } 
-        } while(true);
-        System.out.println("===========================================\n");
+        }
+        System.out.println("============================================\n");
     }
 
-    public static void MarcarAtendimento(Scanner input){
+    public static void MarcarAtendimento(Scanner input) {
         System.out.print("Digite seu CPF:");
         String cpf = input.nextLine();
 
-        if(cpfs.contains(cpf)){
+        if(cpfs.contains(cpf)) {
 
-            do{
+            while (true) {
                 System.out.println("Alguma especialidade em especifico? [S/N]");
                 String res = input.nextLine();
 
-                if(res.equalsIgnoreCase("S")){
-                    do{
+                if(res.equalsIgnoreCase("S")) {
+                    while (true) {
                         System.out.println("Qual?");
                         String res2 = input.nextLine();
 
-                        if(especialidades.contains(res2)){
+                        if(especialidades.contains(res2)) {
                             break;
-                        }else{
+                        } else {
                             System.out.println("Desculpa, ainda não temos essa especialidade! Alguma outra? [S/N]");
                             String res3 = input.nextLine();
-                            if(res3.equalsIgnoreCase("S")){
-                            }else if(res3.equalsIgnoreCase("N")){
+
+                            if (res3.equalsIgnoreCase("S")){
+
+
+                            } else if (res3.equalsIgnoreCase("N")) {
                                 System.out.println("Gostaria de marcar com um clinico geral? [S/N]");
                                 String res4 = input.nextLine();
 
                                 return;
-                            }else{
+                            } else {
                                 System.out.println("Digite uma respota valida");
                             }
                         }
-                    }while (true);
-                }else if(res.equalsIgnoreCase("N")){
+                    }
+                } else if (res.equalsIgnoreCase("N")) {
                     System.out.println("Quais desses medicos você gostaria então?");
-                    for(Medico medico: medicos){
+                    for(Medico medico: medicos) {
                         System.out.println(medico.toString());
                     }
                     String res6 = input.nextLine();
                     return;
                 }
-            }while (true);
-        }else{
+            }
+        } else {
             System.out.println("Desculpa! Não encontramos seu cpf! Gostaria de se cadastrar? [S/N]");
             String res5 = input.nextLine();
 
-            if(res5.equalsIgnoreCase("S")){
+            if (res5.equalsIgnoreCase("S")) {
                 CadastrarPaciente(input);
             }
-            else{
+            else {
                 return;
             }
 
         }
-
-
-
     }
 
-    public static void ModoMedico(Scanner input){
-        while(true){
-            System.out.println("=======MODO MEDICO=======");
+    public static void ModoMedico(Scanner input) {
+        while(true) {
+            System.out.println("======= MODO MEDICO =======");
             System.out.println("Cadastrar Medico - 1");
             System.out.println("Concluir Consulta - 2");
             System.out.println("Cancelar Consulta - 3");
@@ -213,7 +209,7 @@ public class Main {
             int valor = input.nextInt();
             input.nextLine();
 
-            switch (valor){
+            switch (valor) {
                 case 1:
                     System.out.println("Medico cadastrado");
                     break;
