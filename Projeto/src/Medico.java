ddbc57bca1662e304fe55263a70e4c32d8782ab9
Jorgehtg <jgargalhone@gmail.com>
2025-10-07 10:54:40 -0300

    tive que mudar o metodo de verificar o horario livre pois estava dando erro

--- Projeto/src/Medico.java
+++ Projeto/src/Medico.java
@@ -72,20 +72,29 @@ public class Medico{
         this.agenda.remove(horarioRemover);
     }
 
-    public boolean isHorarioLivre(String data, String hora){
+    public boolean isHorarioLivre(int dia, int mes, int ano, int hora, int minutos){
             if (agenda == null || agenda.isEmpty()){
                 return true;
             }
-            for (String atendimentos: agenda){
+            for (String atendimentos : agenda){
                 String[] dados = atendimentos.split(";");
 
                 String dataOcupada = dados[1].trim();
                 String horaOcupada = dados[2].trim();
 
-                if (dataOcupada.equalsIgnoreCase(data) && horaOcupada.equalsIgnoreCase(hora)){
+                String[] data = dataOcupada.split("/");
+                String[] horaLista = horaOcupada.split(":");
+
+                String diaS = String.valueOf(dia);
+                String mesS = String.valueOf(mes);
+                String anoS = String.valueOf(ano);
+                String horaS  = String.valueOf(hora);
+                String minutosS = String.valueOf(minutos);
+
+                if (diaS.equals(data[0]) && mesS.equals(data[1]) && anoS.equals(data[2]) && horaS.equals(horaLista[0]) && minutosS.equals(horaLista[1])){
                     return false;
-                } 
-            }
+                }
+            } 
             return true;
     }
     
